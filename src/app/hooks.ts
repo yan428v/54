8e095c2d7145088ee6d4a26a5d70f672b99ceb29
Yan Vorobev <135717041+yan428v@users.noreply.github.com>
2024-06-18 23:58:52 +0300

    Update hooks.ts

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -10,3 +10,6 @@ import type { AppDispatch, RootState } from "./store"
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
 export const useAppSelector = useSelector.withTypes<RootState>()
+
+
+
